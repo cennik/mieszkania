ba660478ade6534dc5b417bf854caceb36382635
etiaro <j.klimek2002@gmail.com>
2021-07-01 13:52:48 +0200

    updating, new vals

--- src/types.ts
+++ src/types.ts
@@ -3,16 +3,41 @@ import { Set } from 'json-set-map'
 export enum ShopId {
     other = -1,
     Szybkopl = 0,
- };
+};
+export enum MieszkanieState {
+    waiting = 0,
+    good = 1,
+    bin = -1,
+}
 
 export interface Mieszkanie {
     name: string,
     url: string,
     keywords: Set<string>,
+    shopId: ShopId,
+    state: MieszkanieState,
+    price?: number,
+    bedrooms?: number,
+    livingroom?: boolean,
+    eatplace?: boolean,
+    balcony?: boolean,
+    basement?: boolean,
+    PWdist?: number,
+    MiMdist?: number,
+    PWtime?: number,
+    MIMtime?: number,
+    rating?: {
+        kuba?: number,
+        tymek?: number,
+        szymon?: number,
+    },
+    comment?: string,
 }
 
-export const EmptyMieszkanie:Mieszkanie = {
+export const EmptyMieszkanie: Mieszkanie = {
     name: '',
     url: '',
-    keywords: new Set([])
+    keywords: new Set([]),
+    shopId: ShopId.other,
+    state: -1,
 }
--- src/webapi.ts
+++ src/webapi.ts
@@ -1,25 +1,34 @@
+import { Set } from "json-set-map";
 import * as socketio from "socket.io";
 import { Mieszkanie } from "./types";
 
 export default class WebApi {
-    DATA:Map<string, Mieszkanie>;
+    DATA: Map<string, Mieszkanie>;
     io: socketio.Server;
-    constructor(DATA:Map<string, Mieszkanie>){
+    constructor(DATA: Map<string, Mieszkanie>) {
         this.DATA = DATA;
         this.io = new socketio.Server(12345, {
             cors: {
-              origin: "http://etiaro.tk:2137",
-              methods: ["GET", "POST"]
+                origin: "http://etiaro.tk:2137",
+                methods: ["GET", "POST"]
             }
         })
-        this.io.on('connection', (client) => { 
+        this.io.on('connection', (client) => {
             client.emit('allData', DATA);
+            client.on('update', entry => this.updateEntry(entry));
         });
     }
-    updateEntry(entry:Mieszkanie){
+    forceTypes(entry: Mieszkanie) {
+        entry.keywords = new Set(entry.keywords)
+    }
+    updateEntry(entry: Mieszkanie) {
+        this.forceTypes(entry);
+        this.DATA.set(entry.url, entry);
         this.io.emit('update', entry);
     }
-    addEntry(entry:Mieszkanie){
+    addEntry(entry: Mieszkanie) {
+        this.forceTypes(entry);
+        this.DATA.set(entry.url, entry);
         this.io.emit('add', entry);
     }
 }
